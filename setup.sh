e15b000c0b36c01274b9bdb85cb07a7643548704
Võ Bảo <87627629+kenegdane@users.noreply.github.com>
2021-11-07 08:58:58 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+sudo apt update && sudo apt upgrade
+sudo apt install golang git
+git clone https://github.com/kenegdane/go-ddoser
+go get "github.com/gamexg/proxyclient"
+go mod init go-ddoser
+go mod tidy
+go build go-ddoser.go
